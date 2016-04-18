b28c2ff70a177e14ff310ec32927a186d4caad96
Vincent <vincent.elcrin@gmail.com>
2016-04-18 13:18:34 +0200

    Resolve test

--- src/domain/core/message.js
+++ src/domain/core/message.js
@@ -3,82 +3,84 @@ var valueType = require('../../valueType');
 var decisionProjection = require('../decisionProjection');
 var _ = require('lodash');
 
-var MessageId = exports.MessageId = valueType.extends(function MessageId(id){
-    this.id = id;
+var MessageId = exports.MessageId = valueType.extends(function MessageId(id) {
+  this.id = id;
 }, function toString() {
-    return 'Message:' + this.id;
+  return 'Message:' + this.id;
 });
 
-var MessageQuacked = exports.MessageQuacked = function MessageQuacked(messageId, author, content){
-    this.messageId = messageId;
-    this.author = author;
-    this.content = content;
+var MessageQuacked = exports.MessageQuacked = function MessageQuacked(messageId, author, content) {
+  this.messageId = messageId;
+  this.author = author;
+  this.content = content;
 
-    Object.freeze(this);
+  Object.freeze(this);
 };
 
-MessageQuacked.prototype.getAggregateId = function getAggregateId(){
-    return this.messageId;
+MessageQuacked.prototype.getAggregateId = function getAggregateId() {
+  return this.messageId;
 };
 
-var MessageRequacked = exports.MessageRequacked = function MessageRequacked(messageId, requacker){
-    this.messageId = messageId;
-    this.requacker = requacker;
+var MessageRequacked = exports.MessageRequacked = function MessageRequacked(messageId, requacker) {
+  this.messageId = messageId;
+  this.requacker = requacker;
 
-    Object.freeze(this);
+  Object.freeze(this);
 };
 
-MessageRequacked.prototype.getAggregateId = function getAggregateId(){
-    return this.messageId;
+MessageRequacked.prototype.getAggregateId = function getAggregateId() {
+  return this.messageId;
 };
 
-var MessageDeleted = exports.MessageDeleted = function MessageDeleted(messageId){
-    this.messageId = messageId;
+var MessageDeleted = exports.MessageDeleted = function MessageDeleted(messageId) {
+  this.messageId = messageId;
 
-    Object.freeze(this);
+  Object.freeze(this);
 };
 
-MessageDeleted.prototype.getAggregateId = function getAggregateId(){
-    return this.messageId;
+MessageDeleted.prototype.getAggregateId = function getAggregateId() {
+  return this.messageId;
 };
 
-var Message = function Message(events){
-    var self = this;
+var Message = function Message(events) {
+  var self = this;
 
-    var projection = decisionProjection.create()
-      .register(MessageQuacked, function(event) {
-        this.messageId = event.messageId;
-        this.author = event.author;
-      })
-      .register(MessageRequacked, function(event) {
-        if (!this.requackers) {
-          this.requackers = [];
-        }
-        this.requackers.push(event.requacker);
-      })
-      .apply(events);
+  var projection = decisionProjection.create()
+    .register(MessageQuacked, function(event) {
+      this.messageId = event.messageId;
+      this.author = event.author;
+    })
+    .register(MessageRequacked, function(event) {
+      if (!this.requackers) {
+        this.requackers = [];
+      }
+      this.requackers.push(event.requacker);
+    })
+    .apply(events);
 
-    self.requack = function requack(publishEvent, requacker) {
-        if(projection.author.equals(requacker) || _.includes(projection.requackers, requacker)){
-            return;
-        }
+  self.requack = function requack(publishEvent, requacker) {
+    if (projection.author.equals(requacker) || _.includes(projection.requackers, requacker)) {
+      return;
+    }
 
-        publishEvent(new MessageRequacked(projection.messageId, requacker));
-    };
+    publishEvent(new MessageRequacked(projection.messageId, requacker));
+  };
 
-    self.delete = function(publishEvent) {
-        publishEvent(new MessageDeleted(projection.messageId));
-    };
+  self.delete = function(publishEvent, deleter) {
+    if (projection.author.equals(deleter)) {
+      publishEvent(new MessageDeleted(projection.messageId));
+    }
+  };
 };
 
-exports.quack = function quack(publishEvent, author, content){
-    var messageId = new MessageId(idGenerator.generate());
+exports.quack = function quack(publishEvent, author, content) {
+  var messageId = new MessageId(idGenerator.generate());
 
-    publishEvent(new MessageQuacked(messageId, author, content));
+  publishEvent(new MessageQuacked(messageId, author, content));
 
-    return messageId;
+  return messageId;
 };
 
-exports.create = function create(events){
-    return new Message(events);
+exports.create = function create(events) {
+  return new Message(events);
 };
